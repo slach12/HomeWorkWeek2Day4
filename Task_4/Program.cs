489a4469533b9dc9c7994ecf30b5f7605ef918d7
slach12 <slach12@wp.pl>
2023-09-22 00:11:48 +0200

    Zadanie 4 z zmiennym

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 10;
+            string var2 = "Szkoła Dotneta";
+            double var3 = 12.5;
+            Console.WriteLine(var1+" "+var2+" "+var3);
+        }
+    }
+}
