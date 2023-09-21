e676202e31ac40ddd3943f4e524f25f5bd360526
slach12 <slach12@wp.pl>
2023-09-22 00:04:39 +0200

    Zadanie 3 z długością przekątnej.

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Transactions;
+
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość pierwszego boku");
+            double bok1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj długość drugiego boku");
+            double bok2 = double.Parse(Console.ReadLine());
+
+            double diagonal = Math.Sqrt( Math.Pow(bok1,2) + Math.Pow(bok2, 2));
+            Console.WriteLine("Przekątna prostokąta ma miarę : " + diagonal);
+
+        }
+    }
+}
