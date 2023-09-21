20eab33ec237ddddbb45b4accd645b9e00ae87c7
slach12 <slach12@wp.pl>
2023-09-21 23:45:04 +0200

    Dodaj pliki projektów.

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Sławek";
+            string surname = "Łach";
+            int age = 39;
+            char sex = 'm';
+            string personalId = "1234567890";
+            uint idWorker = 2509324094;
+
+            Console.Write("Imie :");
+            Console.WriteLine(name);
+            Console.Write("Nazwisko :");
+            Console.WriteLine(surname);
+            Console.Write("Wiek :");
+            Console.WriteLine(age);
+            Console.Write("Płeć :");
+            Console.WriteLine(sex);
+            Console.Write("PESEl :");
+            Console.WriteLine(personalId);
+            Console.Write("Numer pracownika :");
+            Console.WriteLine(idWorker);
+        }
+    }
+}
