0421caca92cf4c0214dcb73cd448fea3d29e092b
slach12 <slach12@wp.pl>
2023-09-21 23:53:44 +0200

    Zadanie drugie , literki w odwrotnej kolejności.

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first  = 'A';
+            char second = 'B';
+            char third  = 'C';
+
+            Console.WriteLine(third+" "+second+" "+first);
+        }
+    }
+}
